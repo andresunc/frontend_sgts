9571fab667a3ef9dae252e395e45e44d8c394400
Andrés <andres030685@gmail.com>
2024-01-07 19:49:51 -0300

    Agrego Angular Material
    Además agregué estilos al contenedor de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Componentes agregados
 import { SidebarComponent } from 'src/sidebar/sidebar.component';
+
+// Angular Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,8 @@ import { SidebarComponent } from 'src/sidebar/sidebar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
