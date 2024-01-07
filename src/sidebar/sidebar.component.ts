b01c298c018689c5596f26c3a1840808da5da86c
Andrés <andres030685@gmail.com>
2024-01-07 00:45:05 -0300

    Estilos al slidebar component

--- src/sidebar/sidebar.component.ts
+++ src/sidebar/sidebar.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  /**
+   * Este array simula valores que podemos traer de la db para almacenarlos en la LS del usuario, de esta manera vamos a poder implementar una lógica que nos acomode los botones automáticamente.
+   */
   botones = [
     { nombre: 'Presupuestados' },
     { nombre: 'Finalizados' },
