41d0547cd6105f1a1e7f39a14c329d9c21a2ddf3
Andrés <andres030685@gmail.com>
2024-02-07 18:10:23 -0300

    Estructura Print Servicios

--- src/app/componentsServicios/print-servicio/print-servicio.component.ts
+++ src/app/componentsServicios/print-servicio/print-servicio.component.ts
@@ -11,11 +11,13 @@ export class PrintServicioComponent {
 
   servicioRecibido: any;
   title: string = 'Información del servicio: ';
+  recurrencia: number;
 
   constructor(private dataShared: DataSharedService, svManager: ManagerService) {
     this.servicioRecibido = this.dataShared.getSharedObject();
     this.title = this.title + this.servicioRecibido.cliente + ' | ' + this.servicioRecibido.tipo;
     this.servicioRecibido.avance = svManager.calcularAvance(this.servicioRecibido);
+    this.recurrencia = this.servicioRecibido.recurrencia;
   }
 
 }
