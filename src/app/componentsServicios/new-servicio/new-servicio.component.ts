bbb4bc5389da7bfd5afd8598cbfd90e7c063f99d
Andrés <sergio.andres.garcia@mi.unc.edu.ar>
2024-02-23 20:52:52 -0300

    Agrego funcionalidad a New Servicio

--- src/app/componentsServicios/new-servicio/new-servicio.component.ts
+++ src/app/componentsServicios/new-servicio/new-servicio.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-new-servicio',
   templateUrl: './new-servicio.component.html',
   styleUrls: ['./new-servicio.component.css']
 })
-export class NewServicioComponent {
+export class NewServicioComponent implements OnInit {
 
+  servicioForm!: FormGroup;
   title: string = "Nuevo Servicio";
+
+  constructor(private fb: FormBuilder, private cdr: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+    // Inicializar el formulario en el método ngOnInit
+    this.servicioForm = this.fb.group({
+      monto: ['', [Validators.pattern('[0-9]*(\.[0-9]*)?')]],
+      tipo: ['', [Validators.required]],
+      estado: ['', [Validators.required]],
+      responsable: ['', [Validators.required]],
+      empresa: ['', [Validators.required]],
+    });
+  }
+
+  selectedTabIndex: number = 0;
+  goToNextTab(value: number) {
+    this.selectedTabIndex = value
+  }
+
+  tabChanged(event: MatTabChangeEvent) {
+    // Actualizar el índice del tab seleccionado
+    this.selectedTabIndex = event.index;
+  }
+  
 }
