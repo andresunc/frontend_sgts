fbe5d00cce467f56674d7d86b36fba2f40f5676f
Jenifer Alvarez <jniferalvarez@gmail.com>
2024-03-19 20:27:01 -0300

    Cambios configuracion clientes

--- src/app/componentsAdministrador/bodyAdminComponents/cfg-clientes/cfg-clientes.component.ts
+++ src/app/componentsAdministrador/bodyAdminComponents/cfg-clientes/cfg-clientes.component.ts
@@ -1,13 +1,56 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { StepperOrientation } from '@angular/material/stepper';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cfg-clientes',
   templateUrl: './cfg-clientes.component.html',
   styleUrls: ['./cfg-clientes.component.css']
 })
-export class CfgClientesComponent {
+export class CfgClientesComponent implements OnInit {
 
   title: string = "Configuración de Clientes";
-
   
+  firstFormGroup = this._formBuilder.group({
+      RazonSocial: ['', Validators.required],
+      CUIT: ['', Validators.required],
+      Rubro: ['', Validators.required],
+      Riesgo: ['', Validators.required]
+  });
+  secondFormGroup: FormGroup; // Declaración del FormGroup
+  
+  stepperOrientation: Observable<StepperOrientation>;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    this.stepperOrientation = this.breakpointObserver
+      .observe('(min-width: 800px)')
+      .pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical' as StepperOrientation)));
+
+    // Inicializar secondFormGroup en el constructor
+    this.secondFormGroup = this._formBuilder.group({
+    Nombre: ['', Validators.required],
+    Apellido: [''],
+    Direccion: ['', Validators.required],
+    Telefono: ['', Validators.required],
+    Email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  getStepperStyles() {
+    return {
+      'width.%': this.stepperOrientation.pipe(map(orientation => orientation === 'horizontal' ? 100 : 80)),
+      'height.px': this.stepperOrientation.pipe(map(orientation => orientation === 'horizontal' ? 80 : 100))
+    };
+  }
+
+  ngOnInit() {
+    // Agregar el campo de correo electrónico a secondFormGroup
+    this.secondFormGroup.addControl('email', this._formBuilder.control('', [Validators.required, Validators.email]));
+  } 
 }
