221267e244bf69f65308a811468e01e290e46664
Andrés <sergio.andres.garcia@mi.unc.edu.ar>
2024-03-20 17:27:11 -0300

    Inserto Data Source a Rpt0

--- src/app/componentsReportes/bodyRptComponents/servicios-en-curso/servicios-en-curso.component.ts
+++ src/app/componentsReportes/bodyRptComponents/servicios-en-curso/servicios-en-curso.component.ts
@@ -1,33 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { ServicioEnCurso } from 'src/app/models/RptModels/ServicioEnCurso';
+import { ServiciosEnCursoService } from 'src/app/services/RptServices/servicios-en-curso.service';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-servicios-en-curso',
   templateUrl: './servicios-en-curso.component.html',
   styleUrls: ['./servicios-en-curso.component.css']
 })
-export class ServiciosEnCursoComponent {
+export class ServiciosEnCursoComponent implements OnDestroy, OnInit {
 
-  displayedColumns: string[] = ['idServicio', 'empresa', 'rubro', 'tipoServicio', 'dependenciaInvolucrada', 'estado', 'porcentajeAvance'];
-  dataSource = ELEMENT_DATA;
+  serviciosEnCurso: ServicioEnCurso[] = [];
+
+  constructor(private getRpt: ServiciosEnCursoService) { }
+
+  ngOnInit(): void {
+    this.getRpt.getServiciosEnCurso().pipe(takeUntil(this.unsubscribe$))
+      .subscribe((data: ServicioEnCurso[]) => {
+        this.serviciosEnCurso = data;
+      });
+  }
+
+  // Desuscribirse de los observables al destruirse el componente. Evitar probelmas de memoria.
+  private unsubscribe$ = new Subject<void>();
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 
 }
