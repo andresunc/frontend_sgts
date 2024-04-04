3daf5ae0916127dd673cf1bec7be883c4e76ae33
Jenifer Alvarez <jniferalvarez@gmail.com>
2024-04-04 19:24:39 -0300

    cambios en config Menu

--- src/app/ComponentsShared/config-menu/config-menu.component.ts
+++ src/app/ComponentsShared/config-menu/config-menu.component.ts
@@ -1,31 +1,11 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-config-menu',
   templateUrl: './config-menu.component.html',
   styleUrls: ['./config-menu.component.css']
 })
-export class ConfigMenuComponent implements OnInit {
+export class ConfigMenuComponent{
+  
 
-  constructor() { }
-
-  ngOnInit(): void {
-    // Lógica de inicialización aquí
-  }
-
-  @HostListener('window:resize')
-  onResize() {
-    const menuTitle = document.getElementById('menuTitle') as HTMLElement; 
-    const mobileTitle = document.querySelector('.mobile-title') as HTMLElement; 
-    
-    if (menuTitle && mobileTitle) { 
-      if (window.innerWidth <= 768) {
-        menuTitle.style.display = 'none'; 
-        mobileTitle.style.display = 'block';
-      } else {
-        menuTitle.style.display = 'block';
-        mobileTitle.style.display = 'none'; 
-      }
-    }
-  }
-}
+ }
--- src/app/componentsAdministrador/administrador/administrador.component.ts
+++ src/app/componentsAdministrador/administrador/administrador.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfigMenuComponent } from 'src/app/componentsShared/config-menu/config-menu.component';
+import { Subject, takeUntil } from 'rxjs';
+import { DataSharedService } from 'src/app/services/data-shared.service';
+
 
 
 @Component({
@@ -6,9 +11,26 @@ import { Component } from '@angular/core';
   templateUrl: './administrador.component.html',
   styleUrls: ['./administrador.component.css']
 })
-export class AdministradorComponent {
+export class AdministradorComponent implements OnInit {
 
   title: string = "Configuración General";
+  constructor(public dialog: MatDialog,
+    private dataShared: DataSharedService) { }
 
+  ngOnInit(): void {
+    this.dataShared.getListreports().pipe(takeUntil(this.unsubscribe$)).subscribe(() => {
+      this.openRptList();
+    });
+  }
+
+  openRptList() {
+    this.dialog.open(ConfigMenuComponent);
+  }
+
+  private unsubscribe$ = new Subject<void>();
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
   
 }
