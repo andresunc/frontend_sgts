372351a75291c74fdaf55db02a5afab978002f46
Andrés <sergio.andres.garcia@mi.unc.edu.ar>
2024-04-17 18:47:54 -0300

    fix NewServicioService

--- src/app/services/ServiciosDto/ServicioService.ts
+++ src/app/services/ServiciosDto/ServicioService.ts
@@ -7,6 +7,7 @@ import { ItemChecklistDto } from 'src/app/models/ModelsDto/IItemChecklistDto';
 import { PopupService } from '../SupportServices/popup.service';
 import { UrlBackend } from '../../models/Url';
 import { AuthService } from '../auth.service';
+import { NuevoServicioDto } from 'src/app/models/ModelsDto/NuevoServicioDto';
 
 
 @Injectable({
@@ -18,14 +19,15 @@ export class ServicioService {
   urlBackend = new UrlBackend().getUrlBackend();
   private getTopServicesUrl = this.urlBackend + '/servicioDto/getTopServices';
   private getItemsChecklistUrl = this.urlBackend + '/servicioDto/getItemsChecklist';
+  private urlNewServicio = this.urlBackend + '/nuevo/crearServicio';
 
-  constructor(private http: HttpClient, 
-    private _snackBar: PopupService, 
+  constructor(private http: HttpClient,
+    private _snackBar: PopupService,
     private authService: AuthService) { }
 
   // Método para obtener los servicios más recientes
   getTopServices(limit: number): Observable<Servicios[]> {
-    
+
     const headers: HttpHeaders = this.authService.getHeader();
     const params = { limit: limit.toString() };
 
@@ -48,11 +50,8 @@ export class ServicioService {
 
   // Método para obtener los Items del CheckList de un Servicio
   getItemsChecklist(idServicio: number): Observable<ItemChecklistDto[]> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${this.authService.getCurrentToken()}`
-    });
 
+    const headers: HttpHeaders = this.authService.getHeader();
     const params = { idServicio: idServicio.toString() };
 
     return this.http.get<ItemChecklistDto[]>(this.getItemsChecklistUrl, { headers, params })
@@ -66,4 +65,20 @@ export class ServicioService {
         })
       );
   }
+
+  addServicio(addServicio: NuevoServicioDto): Observable<NuevoServicioDto> {
+    
+    const headers: HttpHeaders = this.authService.getHeader();
+    return this.http.post<NuevoServicioDto>(this.urlNewServicio, addServicio, { headers })
+      .pipe(
+        catchError((error) => {
+          if (error instanceof HttpErrorResponse && (error.status === 401 || error.status === 403)) {
+            this.authService.logout();
+          }
+          console.error('Error en la solicitud addServicio', error);
+          return throwError(error);
+        })
+      );
+  }
+
 }
