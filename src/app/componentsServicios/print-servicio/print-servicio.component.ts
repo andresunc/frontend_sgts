e36531ed8f6144b70f4e8efb90b97c96aa23eb38
Andrés <sergio.andres.garcia@mi.unc.edu.ar>
2024-04-18 19:56:25 -0300

    fix popUp delete

--- src/app/componentsServicios/print-servicio/print-servicio.component.ts
+++ src/app/componentsServicios/print-servicio/print-servicio.component.ts
@@ -1,18 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { AddItemComponent } from 'src/app/componentsShared/add-item/add-item.component';
 import { MaterialModule } from 'src/app/componentsShared/material/material.module';
 import { ContactoEmpresa } from 'src/app/models/DomainModels/ContactoEmpresa';
 import { Servicios } from 'src/app/models/DomainModels/Servicios';
-import { ItemChecklistDto } from 'src/app/models/ModelsDto/IItemChecklistDto';
 import ManagerService from 'src/app/services/SupportServices/ManagerService';
 import { PopupService } from 'src/app/services/SupportServices/popup.service';
 import { DataSharedService } from 'src/app/services/data-shared.service';
 import { PrintService } from 'src/app/services/print.service';
 import { EditorComponent } from './editor/editor.component';
-import { DeleteServicioComponent } from './delete-servicio/delete-servicio.component';
 import { ChecklistComponent } from './checklist/checklist.component';
+import { ServicioEmpresaService } from 'src/app/services/DomainServices/servicio-empresa.service';
+import { Router } from '@angular/router';
+import { DeletePopupComponent } from 'src/app/componentsShared/delete-popup/delete-popup.component';
 
 @Component({
   selector: 'app-print-servicio',
@@ -25,13 +25,16 @@ export class PrintServicioComponent implements OnInit {
   title: string = 'Información del servicio: ';
   recurrencia: number = 0;
   contactoEmpresa: ContactoEmpresa[] = [];
-  
+
 
   constructor(
     private dataShared: DataSharedService,
     public dialog: MatDialog,
     private svManager: ManagerService,
     private printService: PrintService,
+    private servicioEmpresa: ServicioEmpresaService,
+    private router: Router,
+    private _snackBar: PopupService
   ) { }
 
   getSvManager() {
@@ -64,7 +67,7 @@ export class PrintServicioComponent implements OnInit {
   editable: boolean = false;
   editarServicio() {
     this.editable = !this.editable;
-    
+
     if (this.editable) console.log('Ahora es posible editar las características de ésta página');
 
     this.dialog.open(EditorComponent, {
@@ -72,10 +75,31 @@ export class PrintServicioComponent implements OnInit {
     });
   }
 
-  eliminarServicio() {
-    this.dialog.open(DeleteServicioComponent, {
-      data: { servicioRecibido: this.servicioRecibido } //
+  checkDelete(): void {
+    const dialogRef = this.dialog.open(DeletePopupComponent, {
+      data: { message: `¿Borrar el servicio ID: ${this.servicioRecibido.idServicio}?` }
     });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.eliminarServicio();
+      } else {
+        console.log('Se canceló la eliminación');
+      }
+    });
+  }
+
+  eliminarServicio() {
+    // Suscripción a Servicio Empresa para eliminar de manera lógica de la bd
+    this.servicioEmpresa.deleteLogico(this.servicioRecibido.idServicio)
+      .subscribe(
+        () => {
+          console.log('ServicioEmpresa Eliminado con éxito:'),
+            this._snackBar.errorSnackBar(`Servicio eliminado ID: ${this.servicioRecibido.idServicio}`),
+            this.router.navigate(['/home']);
+        },
+        (error) => console.error('Error al eliminar el ServicioEmpresa:', error)
+      );
   }
 
   private getContactoEmpresa() {
--- src/app/componentsShared/delete-popup/delete-popup.component.ts
+++ src/app/componentsShared/delete-popup/delete-popup.component.ts
@@ -1,9 +1,5 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { EditorComponent } from 'src/app/componentsServicios/print-servicio/editor/editor.component';
-import { ServicioEmpresaService } from 'src/app/services/DomainServices/servicio-empresa.service';
-import { PopupService } from 'src/app/services/SupportServices/popup.service';
 
 @Component({
   selector: 'app-delete-popup',
