6c5dbb3c8393ccfa21197d5b57674ff717140917
Jenifer Alvarez <jniferalvarez@gmail.com>
2024-05-09 19:15:35 -0300

    capeta item

---
+++ src/app/componentsAdministrador/bodyAdminComponents/cfg-items/cfg-items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cfg-items',
+  templateUrl: './cfg-items.component.html',
+  styleUrls: ['./cfg-items.component.css']
+})
+export class CfgItemsComponent {
+
+}
