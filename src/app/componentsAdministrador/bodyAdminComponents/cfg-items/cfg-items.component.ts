ad53bb430735b138e0c357c47cb320b7ab8df0b3
Jenifer Alvarez <jniferalvarez@gmail.com>
2024-05-11 23:33:17 -0300

    polrizacion de cfg item

--- src/app/componentsAdministrador/bodyAdminComponents/cfg-items/cfg-items.component.ts
+++ src/app/componentsAdministrador/bodyAdminComponents/cfg-items/cfg-items.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable, Subscribable } from 'rxjs';
+import { Observable, Subscribable, map, startWith } from 'rxjs';
+import { Dependencia } from 'src/app/models/DomainModels/Dependencia';
+import { Requisito } from 'src/app/models/DomainModels/Requisito';
 import { Rubro } from 'src/app/models/DomainModels/Rubro';
+import { TipoItem } from 'src/app/models/DomainModels/TipoItem';
+import { TipoServicio } from 'src/app/models/DomainModels/TipoServicio';
+import { DependenciaService } from 'src/app/services/DomainServices/dependencia.service';
+import { RequisitoService } from 'src/app/services/DomainServices/requisito.service';
+import { RubroService } from 'src/app/services/DomainServices/rubro.service';
+import { TipoItemService } from 'src/app/services/DomainServices/tipo-item.service';
+import { TipoServicioService } from 'src/app/services/DomainServices/tipo-servicio.service';
 import { PopupService } from 'src/app/services/SupportServices/popup.service';
 
 @Component({
@@ -11,52 +20,133 @@ import { PopupService } from 'src/app/services/SupportServices/popup.service';
   styleUrls: ['./cfg-items.component.css']
 })
 export class CfgItemsComponent implements OnInit {
-tipo_servicios: any;
-crearItem() {
-throw new Error('Method not implemented.');
-}
-modificarItem() {
-throw new Error('Method not implemented.');
-}
-checkDelete() {
-throw new Error('Method not implemented.');
-}
-modificarEliminarHabilitado: any;
+
+  myControl = new FormControl();
+  displayFn!: ((value: any) => string);
+  filteredOptions?: Observable<Requisito[]>;
+  tipoItems: TipoItem[] = [];
+  dependencias: Dependencia[] = [];
+  rubros: Rubro[] = [];
+  tipoServicios: TipoServicio[] = [];
+  defaultTipoServicio: TipoServicio = new TipoServicio();
+  requisitos: Requisito[] = [];
+  modificarEliminarHabilitado: any;
+
+  firstFormGroup = new FormGroup({
+    nombreItem: new FormControl<string>('', [Validators.maxLength(50)]),
+    tipoServicio: new FormControl<TipoServicio>(new TipoServicio, Validators.required),
+    tipoItem: new FormControl<TipoItem>(new TipoItem, Validators.required),
+    dependencia: new FormControl<Dependencia>(new Dependencia, Validators.required),
+    rubro: new FormControl<Rubro>(new Rubro, Validators.required),
+    cantidad: new FormControl<number>(0 , Validators.required),
+    diasHoras: new FormControl<string>("", Validators.required),
+  });
 
 
   constructor(
     private router: Router,
-    private popupService: PopupService
-  ) {}
+    private popupService: PopupService,
+    private tipoItemService: TipoItemService,
+    private rubroService: RubroService,
+    private dependenciaService: DependenciaService,
+    private tipoServicioService: TipoServicioService,
+    private requisitoService: RequisitoService
+  ) { }
 
-  myControl= new FormControl();
-  displayFn!: ((value: any) => string);
-  filteredOptions?: Observable<undefined>;
-  tipo_items: any;
-  dependencias: any;
-  rubros: any;
+  ngOnInit() : void {
+    this.setParams();
 
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        return value ? this._filter(value) : this.requisitos.slice();
+      })
 
-  firstFormGroup = new FormGroup({
-    nombreItem: new FormControl<string>(''),
-    tipoItem: new FormControl<string>(''),
-    dependencia: new FormControl<string>(''),
-    rubro: new FormControl<Rubro>(new Rubro),
-    
-  });
+    );
+
+  }
   
+  setParams() {
 
-seleccionarRequisito(arg0: any) {
-throw new Error('Method not implemented.');
-}
-onInputFocus() {
-throw new Error('Method not implemented.');
-}
+    this.tipoItemService.getTipoItems()
+    .subscribe(
+      (data) => {
+        this.tipoItems = data;
+        console.log(
+          this.tipoItems,
+        )
+      }
+    )
+
+ 
+    this.rubroService.getAllRubro()
+    .subscribe(
+      (data) => {
+        this.rubros = data;
+        console.log(
+        this.rubros,
+        )
+      }
+    )
+
+    this.dependenciaService.getAll()
+    .subscribe(
+      (data) => {
+        this.dependencias = data;
+        console.log(
+        this.dependencias,
+        )
+      }
+    )
+
+    this.tipoServicioService.getTipoServicesNotDeleted()
+    .subscribe(
+      (data) => {
+        this.tipoServicios = data;
+        this.defaultTipoServicio.idTipoServicio = -99
+        this.defaultTipoServicio.tipoServicio = 'indistinto';
+        this.tipoServicios.push(this.defaultTipoServicio);
+        console.log(
+        this.tipoServicios,
+        )
+      }
+    )
+
+    this.requisitoService.getAll()
+    .subscribe(
+      (data) => {
+        this.requisitos = data;
+        console.log(
+        this.requisitos
+        )
+      }
+    )
+  }
+  
+  seleccionarRequisito(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
 
 
 
-  ngOnInit(){
+  onInputFocus() {
+     this.myControl.setValue('');
+  }
+
+  private _filter(value: string): Requisito[] {
+    const filterValue = value.toLowerCase();
+    return this.requisitos.filter(requisito => requisito.descripcion?.toLowerCase().startsWith(filterValue));
+  }
+
+
+  crearItem() {
+    throw new Error('Method not implemented.');
+  }
+  modificarItem() {
+    throw new Error('Method not implemented.');
+  }
+  checkDelete() {
+    throw new Error('Method not implemented.');
   }
 
 }
-
--- src/app/services/DomainServices/item.service.ts
+++ src/app/services/DomainServices/item.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError, tap, throwError } from 'rxjs';
 import { UrlBackend } from 'src/app/models/Url';
 import { AuthService } from '../auth.service';
 import { Item } from 'src/app/models/DomainModels/Item';
@@ -12,6 +12,9 @@ export class ItemService {
 
   urlBackend = new UrlBackend().getUrlBackend();
   getItemsNotDeleteUrl = this.urlBackend + '/item/getAllNotDeleted';
+  createItemUrl = this.urlBackend + '/item/create';
+  updateItemUrl = this.urlBackend + '/item/update/';
+  deleteItemUrl = this.urlBackend + '/item/delete/';
 
   constructor(private http: HttpClient, private authService: AuthService) { }
 
@@ -19,11 +22,56 @@ export class ItemService {
   getItemsNotDelete(): Observable<Item[]> {
     const headers: HttpHeaders = this.authService.getHeader();
     return this.http.get<Item[]>(this.getItemsNotDeleteUrl, { headers })
+    .pipe(
+      tap(() => {
+      }),
+      catchError((error) => {
+        console.error('Error en la solicitud cargar Items', error);
+        return throwError(error);
+      })
+    );
+  }
+
+  
+  // Método para crear un nuevo Item
+  createItem(newItem: Item): Observable<Item> {
+    const headers: HttpHeaders = this.authService.getHeader();
+    console.log("headers create item: ", headers)
+    console.log("newItem: ", newItem)
+    return this.http.post<Item>(this.createItemUrl, newItem, { headers })
       .pipe(
         catchError((error) => {
-          console.error('Error al cargar los ítems', error);
+          
+          console.error('Error al agregar un nuevo Item', error);
           return throwError(error);
         })
       );
   }
+
+  // Método para modificar un Item
+  public updateItem(idItem: number): Observable<Item> {
+    const headers: HttpHeaders = this.authService.getHeader();
+    return this.http.put<Item>(this.updateItemUrl + idItem, { headers })
+      .pipe(
+        catchError((error) => {
+          
+          console.error('Error al actualizar el Item', error);
+          return throwError(error);
+        })
+      );
+  }
+
+  // delete Item
+  public deleteItem(idItem: number): Observable<void> {
+    const headers: HttpHeaders = this.authService.getHeader();
+    return this.http.delete<void>(this.deleteItemUrl + idItem, { headers })
+      .pipe(
+        catchError((error) => {
+          
+          console.error('Error al eliminar el Item', error);
+          return throwError(error);
+        })
+      );
+  }
+
 }
