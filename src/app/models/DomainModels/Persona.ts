69519a18256d40dc5d96772e316185d71d91f825
Jenifer Alvarez <jniferalvarez@gmail.com>
2024-05-25 09:52:34 -0300

    domain models y service para usuario

---
+++ src/app/models/DomainModels/Persona.ts
@@ -0,0 +1,10 @@
+export class Persona {
+    idPersona?: number;
+    nombre?: string;
+    apellido?: string;
+    dni?: string;
+    telefono?: string;
+    email?: string;
+
+    constructor(){}
+}
---
+++ src/app/models/DomainModels/Usuario.ts
@@ -0,0 +1,8 @@
+export class Usuario {
+    idUsuario?: number;
+    username?: string;
+    password?: string;
+    id_rol?: number;
+
+    constructor(){}
+}
