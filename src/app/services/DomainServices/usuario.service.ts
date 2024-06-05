26bc5421e684a5957effabbc1c0335472526d368
Andrés <sergio.andres.garcia@mi.unc.edu.ar>
2024-06-04 21:04:39 -0300

    Agregro UsuarioDto

--- src/app/services/DomainServices/usuario.service.ts
+++ src/app/services/DomainServices/usuario.service.ts
@@ -1,21 +1,32 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, tap, throwError } from 'rxjs';
 import { UrlBackend } from 'src/app/models/Url';
 import { AuthService } from '../auth.service';
-import { Usuario } from 'src/app/models/DomainModels/Usuario';
+import { Observable, catchError, throwError } from 'rxjs';
+import { UsuarioDto } from 'src/app/models/ModelsDto/UsuarioDto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-private urlBackend = new UrlBackend().getUrlBackend();
-private getUsuarioNotDeleteUrl = this.urlBackend + '/usuario/getAllNotDeleted';
-private createUsuarioUrl = this.urlBackend + '/usuario/create';
-private updateUsuarioUrl = this.urlBackend + '/usuario/update';
-private deleteUsuarioUrl = this.urlBackend + '/usuario/delete/';
+  private urlBackend = new UrlBackend().getUrlBackend();
+  private getUsuarioNotDeleteUrl = this.urlBackend + '/usuario/getAllNotDeleted';
+  private createUserUrl = this.urlBackend + '/usuario/registro';
+  private updateUsuarioUrl = this.urlBackend + '/usuario/update';
+  private deleteUsuarioUrl = this.urlBackend + '/usuario/delete/';
 
-constructor(private http: HttpClient, private authService: AuthService) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
+
+  createUser(newUser: UsuarioDto): Observable<UsuarioDto> {
+    const headers: HttpHeaders = this.authService.getHeader();
+    return this.http.post<UsuarioDto>(this.createUserUrl, newUser, { headers })
+      .pipe(
+        catchError((error) => {
+          console.error('Error al agregar un nuevo usuario', error);
+          return throwError(error);
+        })
+      );
+  }
 
 }
