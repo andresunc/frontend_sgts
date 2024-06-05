0cde9adc0cb30e8b1b657df2a13fe3e890e0b2c6
Andrés <sergio.andres.garcia@mi.unc.edu.ar>
2024-06-04 21:04:39 -0300

    Agregro UsuarioDto

---
+++ src/app/models/ModelsDto/UsuarioDto.ts
@@ -0,0 +1,12 @@
+export class UsuarioDto {
+    username?: string;
+    password?: string;
+    nombre?: string;
+    apellido?: string;
+    dni?: string;
+    telefono?: string;
+    email?: string;
+    rol?: string;
+
+    constructor() {}
+}
