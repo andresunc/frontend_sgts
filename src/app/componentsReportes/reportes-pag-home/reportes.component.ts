d6373ea269a7a05802e2ba3d8934bc15a9c87d42
Jenifer Alvarez <jniferalvarez@gmail.com>
2024-06-19 18:11:06 -0300

    reportes

--- src/app/componentsReportes/reportes-pag-home/reportes.component.ts
+++ src/app/componentsReportes/reportes-pag-home/reportes.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject } from 'rxjs';
 import { DataSharedService } from 'src/app/services/data-shared.service';
 
 @Component({
@@ -12,14 +12,62 @@ export class ReportesComponent implements OnInit {
 
   title: string = "Reportes";
 
-  constructor(public dialog: MatDialog,
-    private dataShared: DataSharedService) { }
-
+  constructor(
+    public dialog: MatDialog,
+    private dataShared: DataSharedService
+  
+  ) { }
+    
   ngOnInit(): void {
     
   }
 
- 
+  @ViewChild('reporte1') Reporte1!: TemplateRef<HTMLElement>;
+  @ViewChild('reporte2') Reporte2!: TemplateRef<HTMLElement>;
+  @ViewChild('reporte3') Reporte3!: TemplateRef<HTMLElement>;
+  @ViewChild('reporte4') Reporte4!: TemplateRef<HTMLElement>;
+
+  openReporte(reportIndex: number) {
+    let selectedTemplate: TemplateRef<HTMLElement>;
+
+    switch (reportIndex) {
+      case 1:
+        selectedTemplate = this.Reporte1;
+        break;
+      case 2:
+        selectedTemplate = this.Reporte2;
+        break;
+      case 3:
+        selectedTemplate = this.Reporte3;
+        break;
+      case 4:
+        selectedTemplate = this.Reporte4;
+        break;
+      default:
+        console.error("Reporte no válido");
+        return;
+    }
+
+    const title = this.getReportTitle(reportIndex);
+    this.dataShared.openDialog(selectedTemplate, title);
+  }
+
+  getReportTitle(reportIndex: number): string {
+    switch (reportIndex) {
+      case 1:
+        return 'Trámites con mayor desvío';
+      case 2:
+        return 'Análisis de Rebote';
+      case 3:
+        return 'Rubros con mayor desvío';
+      case 4:
+        return 'Asignación por tipo de rol';
+      default:
+        return '';
+    }
+  }
+  
+
   // Desuscribirse de los observables al destruirse el componente. Evitar probelmas de memoria.
   private unsubscribe$ = new Subject<void>();
   ngOnDestroy() {
