f340f32bad2b696890f247bb6199dcd5730c6743
Andrés <sergio.andres.garcia@mi.unc.edu.ar>
2024-06-29 17:34:34 -0300

    Insert data para la tracking storage

--- src/app/models/Params.ts
+++ src/app/models/Params.ts
@@ -2,6 +2,7 @@ export class Params {
 
   // Estados
   PRESENTADO = 'Presentado';
+  PRESUPUESTO_RECHAZADO = 'Presupuesto rechazado';
   
   // Categorías
   SIN_INICIAR = 'Sin iniciar';
@@ -13,6 +14,11 @@ export class Params {
   OPE_EXT = 'OPE_EXT';
   OPE_INT = 'OPE_INT';
 
-  //
+  // Contraseña por defecto
   DEFAULT_PASS = '1234'
+
+  // acciones por defecto para la tracking storage
+  CREATE = 'create';
+  UPDATE = 'update';
+  DELETE = 'delete';
 };
